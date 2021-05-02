4d1165b925b4c07fa6ec00fb9d26f4cdbc2cb7bb
Dominic Lee <dominictwlee@gmail.com>
2021-05-02 15:23:45 +0100

    add ext check utils

--- packages/language-server/src/utils.ts
+++ packages/language-server/src/utils.ts
@@ -4,3 +4,9 @@ export function isSubPathOf(parent: string, subPath: string) {
   const relativePath = path.relative(parent, subPath);
   return relativePath && !relativePath.startsWith('..') && !path.isAbsolute(relativePath);
 }
+
+export const isJs = (extName: string): extName is '.js' | '.jsx' =>
+  ['.js', '.jsx'].includes(extName);
+
+export const isTs = (extName: string): extName is '.ts' | '.tsx' =>
+  ['.ts', '.tsx'].includes(extName);
